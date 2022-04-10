b00c674296c8e5443976b8e25816ef16d6fdb194
Bhavesh Shah <bshah@tripactions.com>
2022-04-10 17:34:07 +0200

    added code for split

---
+++ SD/js/footer.js
@@ -0,0 +1,36 @@
+class MyFooter extends HTMLElement {
+    connectedCallback() {
+        fetch('/SD/footer.html').then(data => data.text()
+        ).then(text => {
+            this.innerHTML = text;
+        });
+    }
+}
+
+class FooterScripts extends HTMLElement {
+    connectedCallback() {
+        this.appendChild(
+            this.getScript("https://code.jquery.com/jquery-3.4.1.min.js")
+        );
+        this.appendChild(
+            this.getScript(
+                "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"
+            )
+        );
+        this.appendChild(this.getScript("lib/wow/wow.min.js"));
+        this.appendChild(this.getScript("lib/easing/easing.min.js"));
+        this.appendChild(this.getScript("lib/waypoints/waypoints.min.js"));
+        this.appendChild(this.getScript("lib/owlcarousel/owl.carousel.min.js"));
+        this.appendChild(this.getScript("js/main.js"));
+    }
+
+    getScript(src) {
+        const script = document.createElement("script");
+        script.type = "text/javascript";
+        script.src = src;
+        return script;
+    }
+}
+
+customElements.define("my-footer", MyFooter);
+customElements.define("footer-scripts", FooterScripts);
